6b9f9afe13f753d1f0215d3dfd2b3f253ba595c5
svillar <villar.mucasan@gmail.com>
2024-05-14 23:52:34 -0300

    adicionar bubble sort como leitura de arquivos e escrita de arquivos

--- metodosOrdenacao/prototipos_teste/bubble_sort.cpp
+++ metodosOrdenacao/prototipos_teste/bubble_sort.cpp
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <fstream.h>
+#include <string>
+#include <fstream>
+
 void troca(int* arr, int i, int j)
 {
     int temp = arr[i];
@@ -32,6 +36,34 @@ void mostrarDados(int arr[], int tamanho)
 // funcao teste
 int main()
 {
+    const int MAX_SIZE = 100; // Maximum size of the array
+    int numbers[MAX_SIZE];    // Declare an array to store numbers
+    int count = 0;             // Counter for the number of elements read
+
+    // Specify the relative path to the file
+    string filePath = "../folder_before_current/numbers.txt";
+
+    // Open the TXT file
+    ifstream inputFile(filePath);
+    if (!inputFile) {
+        cerr << "Error: Unable to open file." << endl;
+        return 1;
+    }
+
+    // Read numbers from the file until the end is reached or the array is full
+    while (count < MAX_SIZE && inputFile >> numbers[count]) {
+        count++;
+    }
+
+    // Close the file
+    inputFile.close();
+
+    // Output the numbers read from the file
+    cout << "Numbers read from the file:" << endl;
+    for (int i = 0; i < count; i++) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
     int arr[] = {7,8,912213,1,78,20,29,30,21};
     int tamanho = sizeof(arr) / sizeof(arr[0]);
     bubbleSort(arr, tamanho);
